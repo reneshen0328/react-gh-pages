4a3d23a30b2ef3a8c7e5f6f22cdbbeea0f617826
Rene Shen <rshen@box.com>
2022-10-21 10:08:36 -0700

    im-so-confused

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Third time the charm?
+          Third time the charm??????
         </p>
         <a
           className="App-link"
